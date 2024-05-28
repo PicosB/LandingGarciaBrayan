7ac23cfa5e23fe21a72b9e667c647930d431a52f
PicosB <brayan.garcia232168@potros.itson.edu.mx>
2024-05-28 12:35:32 -0600

    HTML PURO

---
+++ src/main/java/com/mycompany/asignacion1_232168/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.asignacion1_232168;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
